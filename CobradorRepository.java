57122b75424275d4499ab4564bdb8cc9c1c906d6
Lena Diene <60784330+lenadiene@users.noreply.github.com>
2024-03-20 01:51:55 +0000

    add diretórios

---
+++ CobradorRepository.java
@@ -0,0 +1,61 @@
+public class CobradorRepository {
+    
+    private static List<Cobrador> cobradores;
+    
+    static{
+        cobradores = new ArrayList<>();
+    }
+    
+    public static void create(Cobrador b){
+        CobradorRepository.cobradores.add(a);
+    }
+    
+    public static void update(Cobrador b){
+        
+        for(Cobrador bAux: cobradores){
+            
+            if(bAux.getCodigo() == b.getCodigo()){
+                bAux.setCodNome(b.getCodNome());
+                bAux.setDescricao(b.getBreveDescricao());
+                bAux.setEmail(b.getEmail());
+                bAux.setSenha(b.getSenha());
+                bAux.setHabilidades(b.getHabilidades());
+                bAux.setFerramentas(b.getFerramentas());
+            bAux.setValor(b.getValor());
+                
+                return;
+            }
+            
+        }
+        
+    }
+    
+    public static Cobrador read(int codigo){
+        
+        for(Cobrador bAux: cobradores){
+            if(bAux.getCodigo()==codigo){
+                return bAux;
+            }
+        }
+        return null;
+    }
+    
+    public static void delete(int codigo){
+        
+        for(int i = 0; i < cobradores.size();i++){
+            
+            if(cobradores.get(i).getCodigo()==codigo){
+                
+            conradores.remove(i);
+                //cibradores.remove(cobradores.get(i));
+                return;
+            }
+            
+        }
+        
+    }
+    
+    public static List<Cobrador> readAll(){
+return cobradores;
+    }    
+}
--- model/respositories/AgiotaRepository.java
+++ model/respositories/AgiotaRepository.java
@@ -1,18 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.edu.ifpe.recife.model.repositories;
 
-import br.edu.ifpe.recife.model.entities.Agiota;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- *
- * @author ALUNO
- */
 public class AgiotaRepository {
     
     private static List<Agiota> agiotas;
